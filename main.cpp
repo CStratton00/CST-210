1abaf8c64cb639b7d5686cd90ca22931d3018331
Collin Stratton <Collin.Stratton00@gmail.com>
2020-01-09 09:49:37 -0700

    Finds the area of a hexcagon

--- main.cpp
+++ main.cpp
@@ -4,15 +4,25 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 int main() {
-    int lbs = 0;
-    cout << "Please enter your weight in pounds: " << endl;
-    cin >> lbs;
+//    int lbs = 0;
+//    cout << "Please enter your weight in pounds: " << endl;
+//    cin >> lbs;
+//
+//    double kilo = lbs * .454;
+//
+//    cout << "Your weight in kilos is: " << kilo;
 
-    double kilo = lbs * .454;
+    double s = 0.0;
+    cout << "Please enter the side of a hexagon: " << endl;
+    cin >> s;
 
-    cout << "Your weight in kilos is: " << kilo;
+    double area = ((3 * sqrt(3)) / 2) * pow(s, 2);
+
+    cout << "Area of the hexagon is: " << area;
+
     return 0;
 }
